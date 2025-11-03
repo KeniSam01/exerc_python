ae59d9899ee888b4ca989745d759712b88d2a921
João Diniz <kenisam@outlook.com.br>
2025-11-02 21:11:48 -0300

    att

---
+++ aulas/aula_011.py
@@ -0,0 +1,11 @@
+# Como colorir seus codigos com formatação ANSI
+
+print(f"\033[0;30mHello World\033[0;30m")
+print(f"\033[1;31mAqui está com as letras vermelhas\033[1;31m")
+print(f"\033[1;32mAqui esta com as letras verdes\033[1;32m")
+print(f"\033[1;33mAqui está com as letras amarelas\033[1;33m")
+print(f"\033[1;34mAqui está com as letras azuis\033[1;34m")
+print(f"\033[1;35mAqui está com as letras roxas\033[1;35m")
+print(f"\033[1;31;43mAqui as letras estão vermelhas com fundo amarelo\033[1;31;43m")
+print(f"\033[1;32;41mAqui as letras estão verdes com fundo vermelho.\033[1;32;41m")
+print(f"\033[1;34;41mAqui as letras estão azuis com fundo vermelho\033[1;34;41m")
--- exerc_056.py
+++ exerc_056.py
@@ -1,28 +1,47 @@
 # crie um programa que faça o computador jogar jokenpo com você.
 
+import time
 import random
 
 jokenpo = random.choices(["Pedra", "Papel", "Tesoura"], k=3)
 
-usuario = str(input("Vamos jogar JOKENPO!!\nEscolha Pedra, Papel ou Tesoura e veja se consegue ganhar de mim!")).upper().strip()
+usuario = str(input("Vamos jogar JOKENPO!!\nEscolha Pedra, Papel ou Tesoura e veja se consegue ganhar de mim!\nEscolha:")).upper().strip()
 
 if usuario == "PEDRA" and jokenpo[0] == "Pedra":
-    print(f"Você escolheu {usuario} e eu também! EMPATAMOS.")
+    print("Analisando sua escolha...")
+    time.sleep(1.5)
+    print(f"Você escolheu: {usuario}\nE eu também!\n\033[7;30;40mEMPATAMOS.\033[7;30;40m")
 elif usuario == "PEDRA" and jokenpo[0] == "Papel":
-    print(f"Você escolheu {usuario} e eu {jokenpo[0].upper()} GANHEI.")
+    print("Analisando sua escolha...")
+    time.sleep(1.5)
+    print(f"Você escolheu: {usuario}\nE eu: {jokenpo[0].upper()}\n\033[1;31mGANHEI.\033[1;31m")
 elif usuario == "PEDRA" and jokenpo[0] == "Tesoura":
-    print(f"Você escolheu {usuario} e eu {jokenpo[0].upper()} PERDI")
+    print("Analisando sua escolha...")
+    time.sleep(1.5)
+    print(f"Você escolheu: {usuario}\nE eu: {jokenpo[0].upper()}\n\033[1;32mPERDI.\033[1;32m")
 
 elif usuario == "PAPEL" and jokenpo[0] == "Pedra":
-    print(f"Você escolheu {usuario} e eu {jokenpo[0].upper()} PERDI")
+    print("Analisando sua escolha...")
+    time.sleep(1.5)
+    print(f"Você escolheu: {usuario}\nE eu: {jokenpo[0].upper()}\n\033[1;32mPERDI\033[1;32m")
 elif usuario == "PAPEL" and jokenpo[0] == "Papel":
-    print(f"Você escolheu {usuario} e eu também! EMPATAMOS")
+    print("Analisando sua escolha...")
+    time.sleep(1.5)
+    print(f"Você escolheu: {usuario}\nE eu também!\n\033[7;30;40mEMPATAMOS\033[7;30;40m")
 elif usuario == "PAPEL" and jokenpo[0] == "Tesoura":
-    print(f"Você escolheu {usuario} e eu {jokenpo[0].upper()} GANHEI")
+    print("Analisando sua escolha...")
+    time.sleep(1.5)
+    print(f"Você escolheu: {usuario}\nE eu: {jokenpo[0].upper()}\n\033[1;31mGANHEI\033[1;31m")
 
 elif usuario == "TESOURA" and jokenpo[0] == "Tesoura":
-    print(f"Você escolheu: {usuario} e eu também! EMPATAMOS")
+    print("Analisando sua escolha...")
+    time.sleep(1.5)
+    print(f"Você escolheu: {usuario}\nE eu também!\n\033[7;30;40mEMPATAMOS\033[7;30;40m")
 elif usuario == "TESOURA" and jokenpo[0] == "Pedra":
-    print(f"Você escolheu {usuario} e eu {jokenpo[0].upper()} GANHEI")
+    print("Analisando sua escolha...")
+    time.sleep(1.5)
+    print(f"Você escolheu: {usuario}\nE eu: {jokenpo[0].upper()}\n\033[1;31mGANHEI\033[1;31m")
 elif usuario == "TESOURA" and jokenpo[0] == "Papel":
-    print(f"Você escolheu {usuario} e eu {jokenpo[0].upper()} PERDI")
+    print("Analisando sua escolha...")
+    time.sleep(1.5)
+    print(f"Você escolheu: {usuario}\nE eu: {jokenpo[0].upper()}\n\033[1;32mPERDI\033[1;32m")
